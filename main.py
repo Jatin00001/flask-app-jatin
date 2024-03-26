cf95d91865fe701ff19110f202c80dca15a30153
Jatin <datajatin90@gmail.com>
2024-03-26 22:38:27 +0530

    debug

--- main.py
+++ main.py
@@ -213,4 +213,5 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True,port=5000)
+    # app.run(host="0.0.0.0",debug=True,port=5000)
+    app.run(host="0.0.0.0",port=5000)
